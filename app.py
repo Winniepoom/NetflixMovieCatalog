106e88ed3a11503b4a74e8406db85e167cf5ec0d
Pooms <pooms@Poomss-MacBook-Air.local>
2026-08-25 21:31:06 +0700

    edit app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ with open('data/data_movies.json', 'r') as f:
 
 @app.route("/", methods=['GET'])
 def home():
-    return "Hi! This app is an API, there is no UI ;-)"
+    return "Hi! This app is an API, there is no UI ;-) CI/CD is working sucker"
 
 
 @app.route('/discover')
