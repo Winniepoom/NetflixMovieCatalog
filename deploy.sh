1fecceacdbcc0a7c22d064bd69ad8004f11e12e3
Pooms <pooms@Poomss-MacBook-Air.local>
2026-08-25 22:05:28 +0700

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,10 @@
 
 # TODO your deploy script implementation...test
 
-set -e pipefail
-cd ~/NetflixMovieCatalog
-pkill -f "app.py
-nohup ./app.py > output.log 2>&1 &"
+  set -e -o pipefail
+
+  cd "$HOME/NetflixMovieCatalog"
+
+  pkill -f '[p]ython.*app.py' || true
+
+  nohup ./venv/bin/python app.py > output.log 2>&1 < /dev/null &
